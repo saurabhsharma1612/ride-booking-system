7aa9bead9f85291923884a0b0a7070f244280db5
saurabhsharma1612 <saurabh40936@gmail.com>
2026-06-14 23:28:53 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/saurabh/ridebooking/RideBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh.ridebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabh/ridebooking/RideBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.ridebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
